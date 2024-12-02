ee576f4aad02851fae78ed5f0897486f3b80387c
anshul-chaubey <108220763+anshul-chaubey@users.noreply.github.com>
2024-12-02 17:43:41 +0530

    Create Machinecode.java
    java program to implement below pattern
    10101010
    01010101
    10101010
    01010101
    10101010
    01010101
    10101010
    01010101

---
+++ Patterns/Machinecode.java
@@ -0,0 +1,16 @@
+public class Machinecode {
+
+	public static void main(String[] args) {
+		int numTiles=8;
+		for(int i=0; i<numTiles;i++){
+			for(int j=0;j<numTiles;j++){
+				if((i+j)%2==0){
+					System.out.print("1");
+				}else{
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
