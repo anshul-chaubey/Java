4087f6e898c52a8e8e646a7ed1dd136c71e20e65
anshul-chaubey <108220763+anshul-chaubey@users.noreply.github.com>
2025-06-11 23:59:03 +0530

    Update hello.java

--- ElimentaryJava/hello.java
+++ ElimentaryJava/hello.java
@@ -1,1 +1,10 @@
+public class hello{  // if using public keyword class name eg hello needs to be same as filename as hello.java
+    // if want different name like test.java but with hello class inside need to remove public keyword
+    public static void main(String args[]){
+        // String - S is in caps no small letter as it is class/wrapper class in java not a primitive datatype
+        //String primitive counterpart does not exist like int (primitive) for Integer(wrapper class)
 
+        System.out.println("hello my name is anshul and i am learning java!");
+
+    }
+}
